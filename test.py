af4e80b10c5c93d8090581e92885dff58627db23
Swastik <swastilbansal0@gmail.com>
2024-09-09 19:38:54 +0530

    Added the standalone working model

---
+++ test.py
@@ -0,0 +1,139 @@
+from pathlib import Path
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+from numpy import concatenate, argmax, array, expand_dims, zeros
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+import requests
+
+
+class Model:
+    def execute(self):
+        # print("model")
+        mp_holistic = mp.solutions.holistic  # Holistic model
+        mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+        def mediapipe_detection(image, model):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            results = model.process(image)
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            return image, results
+
+        def draw_landmarks(image, results):
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+        def draw_styled_landmarks(image, results):
+            # Draw pose connections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
+                                      )
+            # Draw left-hand connections
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
+                                      )
+            # Draw right-hand connections
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                                      mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2)
+                                      )
+
+
+        def extract_keypoints(results):
+            pose = array([[res.x, res.y, res.z, res.visibility] for res in
+                          results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else zeros(33 * 4)
+            face = array([[res.x, res.y, res.z] for res in
+                          results.face_landmarks.landmark]).flatten() if results.face_landmarks else zeros(468 * 3)
+            lh = array([[res.x, res.y, res.z] for res in
+                        results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else zeros(
+                21 * 3)
+            rh = array([[res.x, res.y, res.z] for res in
+                        results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else zeros(
+                21 * 3)
+            return concatenate([pose, face, lh, rh])
+
+        DATA_PATH = os.path.join('MP_Data')
+
+        # Labels for data
+        actions = array(['hello', 'thanks', 'iloveyou'])
+
+        log_dir = os.path.join('Logs')
+        tb_callback = TensorBoard(log_dir=log_dir)
+
+        # Defining Model
+        model = Sequential()
+        
+        model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
+        model.add(LSTM(128, return_sequences=True, activation='relu'))
+        model.add(LSTM(64, return_sequences=False, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(actions.shape[0], activation='softmax'))
+
+        # Loading model weights
+        model_path = Path(__file__).parent / 'action.h5'
+        model.load_weights(str(model_path))
+
+        # 1. New detection variables
+        sequence = []
+        sentence = []
+        threshold = 0.9
+
+        # cap = cv2.VideoCapture(0) # Default camera
+        cap = cv2.VideoCapture(1) # Secondary camera (Phone camera)
+        
+        with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+            while cap.isOpened():
+
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+
+                draw_styled_landmarks(image, results)
+
+                # 2. Prediction logic
+                keypoints = extract_keypoints(results)
+                sequence.append(keypoints)
+                sequence = sequence[-30:]
+
+                if len(sequence) == 30:
+                    res = model.predict(expand_dims(sequence, axis=0))[0]
+                    # print(actions[np.argmax(res)])
+
+                    # 3. Text Scriptq
+                    if res[argmax(res)] > threshold:
+                        if len(sentence) > 0:
+                            if actions[argmax(res)] != sentence[-1]:
+                                sentence.append(actions[argmax(res)])
+                        else:
+                            sentence.append(actions[argmax(res)])
+
+                    if len(sentence) > 5:
+                        sentence = sentence[-5:]
+
+                cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+                cv2.putText(image, ' '.join(sentence), (3, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+                cv2.imshow('OpenCV Feed', image)
+
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+
+model = Model()
+model.execute()
